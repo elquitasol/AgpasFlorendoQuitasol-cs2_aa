8dc16c3fd63ce9b9d0f3805ed3442c3c493cbdf4
8-Camia <elquitasol@carc.pshs.edu.ph>
2026-03-11 14:05:02 +0800

    All of the group members has contributed to this code. Finished addTask function (Added overdue verifier and task tracker.) Added editTask function

--- Project Code.py
+++ Project Code.py
@@ -2,28 +2,46 @@ import csv
 import datetime
 
 try:
-    with open('task.csv', 'r') as file:
+    filename = 'task.csv'
+    with open(filename, 'r') as file:
         reader = csv.reader(file)
+
+    timex = datetime.datetime.now()
+    timeNow = f"{timex.month}/{timex.day}/{timex.year}"
 
     def addTask():
         name = input("Please enter the name for your task: ")
         category = input("Enter a category for the task (ex. FA, AA, SA): ")
         deadline = input("Enter the deadline in this format, MM/DD/YYYY: ")
         notes = input("Enter the notes for the task: ")
-        with open('task.csv', 'a', newline='') as file:
+        status = False
+        if status == False:
+            print("Task is not finished.")
+        else:
+            print("Task is finished!")
+
+        if deadline < timeNow:
+            task_overdue = True
+        else:
+            task_overdue = False
+
+        with open(filename, 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow([name, category, deadline, notes])
+            writer.writerow([name, category, deadline, notes, task_overdue, status ])
         print("Done!")
-        print("Here are the tasks.")
-        with open('task.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                for item in row:
-                    print(item)
+
+    def editTask():
+        TaskChoice = input("Please enter the name of the task you want to edit: ")
+        for name in filename:
+            if name["Task Name"] == TaskChoice:
+                name["Finished?"] = True
+
 
     choice = 0
+    print("\nWelcome to our Smart Study Planner!")
+    print(f"Today's date is {timex.month}/{timex.day}/{timex.year}")
     while choice != 5:
-        choice = int(input("\nWelcome to our Smart Study Planner! \n \nPlease pick an option.\n1. View Instructions\n2. Add a task.\n3. Edit a task\n4. Delete a task\n4. Exit\n(Currently, viewing instructions and adding a task is the only option available.)\n\nInput your choice here: "))
+        choice = int(input("\nPlease pick an option.\n1. View Instructions\n2. Add a task.\n3. Edit a task\n4. Delete a task\n5. Exit\n(Currently, viewing instructions and adding a task is the only option available.)\n\nInput your choice here: "))
         if choice == 1:
             print(4*"*","INSTRUCTIONS",4*"*")
             print("\nWhat does our code do?\nThis code's purpose is to help students on their studies by tracking their tasks.\nStudents can add a task, edit a task, or delete a task in this code. ")
@@ -31,7 +49,7 @@ try:
         elif choice == 2:
             addTask()
         elif choice == 3:
-            print("\nSorry, this option is not yet available. Please pick option 1, 2 or exit.")
+            editTask()
         elif choice == 4:
             print("\nSorry, this option is not yet available. Please pick option 1, 2 or exit.")
         else:
